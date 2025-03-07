9b5afdb4293407ce3f52b42f782b1ba09a903605
pharn001 <younothing269@gmail.com>
2025-03-07 16:54:31 +0700

    soaa

--- src/app/_component/experence/Experence.jsx
+++ src/app/_component/experence/Experence.jsx
@@ -11,7 +11,60 @@ export default function Experence() {
       <div className={``}>nothing</div>
       <div className={`grid px-2 py-6 grid-cols-[25%_75%] transition-all rounded-sm ${isMouseEnter['A1'] ? "bg-nothing-Purple" : ""}`}
         onMouseEnter={() => setIsMouseEnter(prev => ({ ...prev, A1: true }))}
-        onMouseLeave={() => setIsMouseEnter(prev => ({ ...prev, A1: false }))}
+        onMouseLeave={() => setIsMouseEnter(prev => ({ ...prev, A1: false }))} 
+        id='section-about'
+      >
+        <div>
+         
+            <FormattedDate isHighlight={isMouseEnter['A1']} items="2020-2024" />
+          <Picture props={data.url}/>
+          
+          
+        </div>
+        <div className='grid gap-y-4 '>
+          <div>Fashion Ecommerce</div>
+          <div className='flex gap-4 text-lg'>
+            <GitHubIcon />
+            <GitHubIcon />
+            <GitHubIcon />
+          </div>
+          <div className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi cum quaerat sapiente culpa modi placeat, a sed cupiditate totam ex ab magnam ratione laborum repellendus voluptas? Animi sed nihil omnis.</div>
+          <div className='flex gap-4'>
+            <div>React</div>
+            <div>tiawind</div>
+          </div>
+        </div>
+      </div>
+      <div className={`grid px-2 py-6 grid-cols-[25%_75%] transition-all rounded-sm ${isMouseEnter['A1'] ? "bg-nothing-Purple" : ""}`}
+        onMouseEnter={() => setIsMouseEnter(prev => ({ ...prev, A1: true }))}
+        onMouseLeave={() => setIsMouseEnter(prev => ({ ...prev, A1: false }))} 
+        id='section-project'
+      >
+        <div>
+         
+            <FormattedDate isHighlight={isMouseEnter['A1']} items="2020-2024" />
+          <Picture props={data.url}/>
+          
+          
+        </div>
+        <div className='grid gap-y-4 '>
+          <div>Fashion Ecommerce</div>
+          <div className='flex gap-4 text-lg'>
+            <GitHubIcon />
+            <GitHubIcon />
+            <GitHubIcon />
+          </div>
+          <div className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi cum quaerat sapiente culpa modi placeat, a sed cupiditate totam ex ab magnam ratione laborum repellendus voluptas? Animi sed nihil omnis.</div>
+          <div className='flex gap-4'>
+            <div>React</div>
+            <div>tiawind</div>
+          </div>
+        </div>
+      </div>
+      <div className={`grid px-2 py-6 grid-cols-[25%_75%] transition-all rounded-sm ${isMouseEnter['A1'] ? "bg-nothing-Purple" : ""}`}
+        onMouseEnter={() => setIsMouseEnter(prev => ({ ...prev, A1: true }))}
+        onMouseLeave={() => setIsMouseEnter(prev => ({ ...prev, A1: false }))} 
+        id='section-arcitle'
       >
         <div>
          
--- src/app/_component/nav/nav.jsx
+++ src/app/_component/nav/nav.jsx
@@ -1,16 +1,30 @@
-
+"use client"
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export default function Nav() {
+  const nav = [
+    { title: "About", section_ID: "section-about" },
+    { title: "experience", section_ID: "section-experience" },
+    { title: "project", section_ID: "section-project" },
+    { title: "Arcitle", section_ID: "section-arcitle" },
+  ]
+  const handleclick = (ID) => {
+   document.getElementById(ID)?.scrollIntoView({behavior:"smooth"})
+  }
   return (
     <div className="flex flex-col gap-3 font-semibold">
-    <div>
-      <ArrowForwardIcon />
-      about</div>
-    <div>Experienc</div>
-    <div>project</div>
-    <div>Arcitle</div>
-  </div>
+      {nav.map((element, index) => (
+        <div key={`nav-${element}-${index}`}
+          onClick={() => handleclick(element.section_ID)}
+        >
+          <ArrowForwardIcon />
+          {element.title}
+
+        </div>
+
+      ))}
+
+    </div>
   )
 }
 
